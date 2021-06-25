f125f03b3792cd981e4e3d81b7f56099dea81443
rfalefp <rafaelfepedrosa@gmail.com>
2021-06-25 14:20:49 -0300

    Resposta do desafio proposto.

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Ivory.TesteEstagio.CampoMinado
 {
@@ -11,7 +13,35 @@ namespace Ivory.TesteEstagio.CampoMinado
             Console.WriteLine(campoMinado.Tabuleiro);
 
             // Realize sua codificação a partir deste ponto, boa sorte!
+            var campoMinadoAux = new CampoMinado();
+            String stringTratada;
 
+            int cont = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                for (int y = 1; y < 10; y++)
+                {
+                    campoMinadoAux.Abrir(i, y);                       
+                }
+            }
+
+            stringTratada = campoMinadoAux.Tabuleiro.Replace("\r", "").Replace("\n", "");
+            
+
+            for(int i = 1; i < 10; i++)
+            {
+                for (int y = 1; y < 10; y++)
+                {
+                    if (!stringTratada[cont].ToString().Contains('*'))
+                    {
+                        campoMinado.Abrir(i, y);
+                    }
+                    cont++;
+                }
+            }
+            Console.WriteLine("\nResultado\n=========");
+            Console.WriteLine(campoMinado.Tabuleiro);
+            Console.WriteLine("\nStatus do jogo: " + campoMinado.JogoStatus);
         }
     }
 }
